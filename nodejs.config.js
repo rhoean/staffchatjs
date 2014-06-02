179162719680e425b7822f53fc00e5f672e74a54
rhoean <rhoean@live.com>
2014-06-02 17:16:08 -0500

    New Files Added

---
+++ nodejs.config.js
@@ -0,0 +1,12 @@
+settings = {
+  host: '*',
+  serviceKey: 'CHANGE ME',
+  backend: {
+    port: 80,
+    host: 'CHANGE ME TO THE HOSTNAME OF YOUR DRUPAL SITE',
+    scheme: 'http',
+    basePath: '',
+    messagePath: '/nodejs/message'
+  },
+  debug: true
+};
